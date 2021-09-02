8fa0d82668154ec96616c10c5acc289dc75c56aa
Nivesh-GitHub <89345698+Nivesh-GitHub@users.noreply.github.com>
2021-09-02 10:28:05 +0530

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,21 @@
+score = 0
+questions = 2
+
+print("Welcome to the python quiz") 
+ans = input("how to import turtle in python")
+if ans.lower == "import turtle" #Has to be in lowercase
+score +=1 #Change score
+print("yep")
+else :
+  print("nope")
+
+ans = input("where can u tell your codes ")
+if ans.lower == "github" 
+score +=1 
+print("yep")
+else :
+  print("nope")  
+  
+  
+ score2 = score/questions
+ print("here is your percantage",score2) 
